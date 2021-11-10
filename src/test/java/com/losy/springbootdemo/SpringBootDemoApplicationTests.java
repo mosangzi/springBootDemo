2c1fd8702e9c28f2e8328ffcfd52b42663690c62
mosangzi <zhangxiaokaihit@gmail.com>
2021-11-10 22:44:58 +0800

    use application.yaml to assignment value

--- src/test/java/com/losy/springbootdemo/SpringBootDemoApplicationTests.java
+++ src/test/java/com/losy/springbootdemo/SpringBootDemoApplicationTests.java
@@ -1,13 +1,23 @@
 package com.losy.springbootdemo;
 
+import com.losy.springbootdemo.pojo.Dog;
+import com.losy.springbootdemo.pojo.Person;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SpringBootDemoApplicationTests {
 
+    @Autowired
+    private Person person;
+    @Autowired
+    private Dog dog;
+
     @Test
     void contextLoads() {
+        System.out.println(person);
+        System.out.println(dog);
     }
 
 }
